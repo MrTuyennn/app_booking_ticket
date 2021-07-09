428a48f1b7667388ed14f9b64e356594a3515b52
MrTuyennn <nguyenngoctuyen188@gmail.com>
2021-07-09 15:05:15 +0700

    setUp project

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@apis': './src/apis',
+          '@assets': './src/assets', // optional
+          '@fonts': './src/assets/fonts',
+          '@images': './src/assets/images',
+          '@icons': './src/assets/icons',
+          '@components': './src/components', // optional
+          '@constants': './src/constants', // optional
+          '@screens': './src/screens', // optional
+          '@states': './src/states', // optional
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+};
